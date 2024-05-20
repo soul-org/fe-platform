84448d62a8af5046d423438f4a73cbc83207f64b
joan <joan.flotats@midokura.com>
2024-05-20 19:27:31 +0200

    feat(pool): add pool page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
+import App from "./App";
 
 // biome-ignore lint: Ignore noNullAssertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
