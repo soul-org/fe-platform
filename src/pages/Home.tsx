0faeee0b23a85d2119d3c82e8aad801d8bb5cccf
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-28 00:48:01 +0300

    chore: Add Header component to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <div className="w-full">
       <Navbar />
+      <Header />
     </div>
   )
 }
