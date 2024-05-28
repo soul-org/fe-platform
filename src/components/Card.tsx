ea57b5f50a7fa0d15bc03aaf9e3c0b385fc07c5a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-28 19:58:07 +0300

    chore: Update Card component styling and structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,9 +4,10 @@ interface CardProps {
   description: string;
 }
 
-const Card: React.FC<CardProps> = ({logo, title, description}) => {
+const Card: React.FC<CardProps> = ({ logo, title, description }) => {
   return (
-    <div className="p-8 backdrop-blur-md bg-[#100F0FCC] before:absolute before:top-20 before:inset-0 before:w-3/4 before:h-3/4 before:rounded-full before:bg-gradient-to-br before:from-[#100F0FCC] before:via-[#100F0FCC] before:to-red-700 before:blur-lg rounded-md before:ring-white border">
+    <div className="relative p-8 bg-gradient-to-br from-[#100F0F] to-[#232323] shadow-2xl rounded-md border border-gray-800">
+      <div className="absolute top-20 h-1/2 w-full inset-0 bg-gradient-to-br from-transparent via-red-500 to-transparent blur-lg rounded-full opacity-50"></div>
       <div className="flex gap-3 items-center my-3">
         <img src={logo} alt="Logo" />
         <h2 className="text-xl">{title}</h2>
@@ -16,4 +17,4 @@ const Card: React.FC<CardProps> = ({logo, title, description}) => {
   )
 }
 
-export default Card
+export default Card;
