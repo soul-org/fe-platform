cfa4a27085d2d40ee10554bec705aeaf8fd32273
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-28 19:58:21 +0300

    chore: Update Header component styling and structure

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -21,8 +21,8 @@ const arrayCard = [
 
 const Header = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-12'>
-      <h2 className='text-3xl my-3'>No Loss Prize Savings Protocol</h2>
+    <div className='flex flex-col items-center justify-center my-20'>
+      <h2 className='text-6xl my-3 font-bold'><span className="text-orange-600">No Loss</span> Prize Savings Protocol</h2>
       <div className='flex items-center justify-center gap-3 my-6'>
         {arrayCard.map((card, index) => (
             <Card key={index} logo={card.logo} title={card.title} description={card.description} />
