99aaeaf97ed80fb7cc6b6b6b8c382542a17d85ce
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-31 08:27:05 +0300

    chore: Update Navbar component styling and structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-import Button from "./Button"
+import Button from "./Button";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 rounded-lg mx-4 my-2">
       <img src="/logo.png" alt="logo" className="h-10" />
       <div className="flex space-x-4">
-        <Button onclick={() => {}} className="bg-orange-500 p-2 rounded">Connect wallet</Button>
+        <Button onclick={() => {}} className="bg-orange-500 p-2 rounded">
+          Connect wallet
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
