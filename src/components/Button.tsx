7268e25c2db84d59d5a0615ffe214f2f6d7db6a6
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-31 08:25:07 +0300

    chore: Update Button component to use React.FC type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,9 @@
-import React, { FC } from "react";
-
 interface ButtonProps {
   className?: string;
   onclick: () => void;
   children: React.ReactNode
 }
-const Button: FC<ButtonProps> = ({className, onclick, children}) => {
+const Button: React.FC<ButtonProps> = ({className, onclick, children}) => {
   return (
     <button className={`px-2 py-1, ${className}`} onClick={onclick}>{children}</button>
   )
